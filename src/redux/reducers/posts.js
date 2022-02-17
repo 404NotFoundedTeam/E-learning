2b0f5a73aa40f9d651f12041955db087b9d4e98a
AliwerDev <alwerdev@gmail.com>
2022-02-17 19:07:38 +0500

    redux ishlandi

---
+++ src/redux/reducers/posts.js
@@ -0,0 +1,16 @@
+import * as t from "../types";
+
+const initialState = { 
+
+
+};
+
+const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default postsReducer;
