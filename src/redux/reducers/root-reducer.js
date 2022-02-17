f679936e9c40900e1357bc6e5fb966d485e40e48
AliwerDev <alwerdev@gmail.com>
2022-02-17 19:00:34 +0500

    alisher

--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import globalReducer from "./global";
+import postsReducer from "./posts";
 import todosReducer from "./todos";
 
 const rootReducer = combineReducers({
   global: globalReducer,
   todos: todosReducer,
+  posts: postsReducer,
 });
 
 export default rootReducer;
