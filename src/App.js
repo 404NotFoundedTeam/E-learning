744338382ad62cdfc8798cf4dd8e246ef91de9ae
AliwerDev <alwerdev@gmail.com>
2022-02-18 10:40:23 +0500

    hello world

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<p className="text-5xl">Hello world</p>} />
+      <Route path="/posts" element={<Posts />} />
     </Routes>
   );
 }
