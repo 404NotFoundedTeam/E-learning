a1095dc389bba7855844b484003d9d9b8475ef30
na-pydev <nurmatov.abdurahim98@mail.ru>
2022-02-19 08:16:52 +0500

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Courses from "./pages/courses";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/course" element={<HomePage />} />
-      <Route path="/posts" element={<HomePage />} />
-    </Routes>
+    <>
+      <div className='header-cover sticky top-0 left-0 w-full shadow-sm'>
+        <Header />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course" element={<HomePage />} />
+        <Route path="/posts" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
