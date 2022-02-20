42a56834508d239f508d317a6b0609cf919f34a9
galabeketov <galabeketov>
2022-02-20 19:37:15 +0500

    cheking

--- src/pages/course/CardMain.js
+++ src/pages/course/CardMain.js
@@ -5,44 +5,96 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-)
+import {
+  FaArrowDown,
+  FaBorderAll,
+  FaCertificate,
+  FaDeskpro,
+  FaFile,
+} from 'react-icons/fa'
+import MainButton from '../../components/Button'
 
 const card = (
-  <React.Fragment>
+  <>
     <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
+      <Typography
+        sx={{ fontSize: '1.625rem', fontWeight: 'bold' }}
+        color="text.secondary"
+        gutterBottom
+      >
+        $269.99
       </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
+      <Typography sx={{ mb: 3 }} color="text.primary">
+        This course includes:
       </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
+      <div className="flex flex-col gap-5">
+        <Typography
+          className="flex items-center gap-2 "
+          sx={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '400' }}
+          variant="body2"
+        >
+          <FaFile /> 9 Lessons
+        </Typography>
+
+        <Typography
+          className="flex items-center gap-2 "
+          sx={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '400' }}
+          variant="body2"
+        >
+          <FaArrowDown /> 12 Downloadable resources
+        </Typography>
+
+        <Typography
+          className="flex items-center gap-2 "
+          sx={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '400' }}
+          variant="body2"
+        >
+          <FaDeskpro /> Access on desktops, tablets, mobile
+        </Typography>
+
+        <Typography
+          className="flex items-center gap-2 "
+          sx={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '400' }}
+          variant="body2"
+        >
+          <FaBorderAll /> Full lifetime access
+        </Typography>
+
+        <Typography
+          className="flex items-center gap-2 "
+          sx={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '400' }}
+          variant="body2"
+        >
+          <FaCertificate /> Certificate of completion
+        </Typography>
+      </div>
     </CardContent>
     <CardActions>
-      <Button size="small">Learn More</Button>
+      <MainButton
+        variant={'contained'}
+        children={'Enter Now'}
+        className="h-14 w-full transition-colors"
+        sx={{ hover: { backgroundColor: 'green' } }}
+      />
     </CardActions>
-  </React.Fragment>
+  </>
 )
 
 export default function CardMain() {
   return (
     <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
+      <Card
+        sx={{
+          padding: '30px',
+          color: 'rgb(33, 43, 54)',
+          backgroundColor: 'rgb(255, 255, 255)',
+          boxShadow: 'rgb(145 158 171 / 16%) -16px 16px 56px -8px',
+          borderRadius: '16px',
+          overflow: 'hidden',
+        }}
+      >
+        {card}
+      </Card>
     </Box>
   )
 }
