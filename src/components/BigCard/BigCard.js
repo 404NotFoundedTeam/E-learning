e683380664accc457cee9b4b77897437b30a78bf
Salaudev <pysalau2909@gmail.com>
2022-02-21 06:55:26 +0500

    Navlink and oldPrice

--- src/components/BigCard/BigCard.js
+++ src/components/BigCard/BigCard.js
@@ -3,10 +3,11 @@ import { Avatar } from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import numeral from "numeral";
+import { NavLink } from "react-router-dom";
 
 export default function BigCard({ obj }) {
   return (
-    <div className="big-card flex w-full rounded-t-md rounded-l-3xl overflow-hidden h-90 mb-8">
+    <div className="big-card flex w-full rounded-2xl  overflow-hidden h-90 mb-8">
       <div className="left w-4/12 h-full">
         <img
           src={obj.img}
@@ -18,12 +19,30 @@ export default function BigCard({ obj }) {
       <div className="right p-6 w-8/12 h-full flex flex-col bg-white justify-around">
         <div>
           <div className="flex justify-between">
-            <h3 className="text-orange-400 text-sm font-weight-500">
+            <h3
+              className=" text-sm font-bold"
+              style={{ color: "rgb(250, 84, 28)", fontSize: "0.75rem" }}
+            >
               {obj.type.toUpperCase()}
             </h3>
-            <p className="text-slate-700 font-bold text-2xl">${obj.price}</p>
+            <p
+              className="font-bold text-2xl"
+              style={{
+                color: "rgb(33, 43, 54)",
+                boxShadow: "rgb(145 158 171 / 16%) -16px 16px 56px -8px",
+              }}
+            >
+              {obj.oldPrice && (
+                <del className="mr-3 text-xl">${obj.oldPrice}</del>
+              )}{" "}
+              ${obj.price}
+            </p>
           </div>
-          <h3 className="text-slate-800 pb-2 font-bold text-md">{obj.name}</h3>
+          <NavLink to={`${obj.name}`}>
+            <h3 className="text-slate-800 pb-2 font-bold text-md">
+              {obj.name}
+            </h3>
+          </NavLink>
           <p className="text-sm  text-slate-400">{obj.description}</p>
         </div>
         <div className="flex flex-col gap-2 w-full">
--- src/components/mini-components/Chip/chip.js
+++ src/components/mini-components/Chip/chip.js
@@ -2,20 +2,14 @@ import * as React from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 
-export default function DeletableChips({ arr }) {
+export default function DeletableChips({ item }) {
   const handleDelete = () => {
     console.info("You clicked the delete icon.");
   };
 
   return (
-    <div>
-      {arr.map((item, i) => (
-        <span>
-          <Stack direction="row" spacing={1} sx={{}}>
-            <Chip label={item} variant="outlined" onDelete={handleDelete} />
-          </Stack>
-        </span>
-      ))}
-    </div>
+    <Stack direction="row" spacing={1} sx={{}}>
+      <Chip label={item} variant="outlined" onDelete={handleDelete} />
+    </Stack>
   );
 }
