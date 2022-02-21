4c7a74eb8e3e6af571def0ade7697f93fcef95de
galabeketov <galabeketov>
2022-02-22 01:53:34 +0500

    asa

--- src/pages/course/DialodCourse.js
+++ src/pages/course/DialodCourse.js
@@ -63,19 +63,23 @@ export default function DialogCourse() {
 
   return (
     <div>
-      <MainButton
-        onClick={handleClickOpen}
-        startIcon={<FaPen />}
-        children="Whrite A Review"
-        sx={{
-          width: '100%',
-          height: '50px',
-          color: 'rgb(250, 84, 28)',
-          '&:hover': {
-            boxShadow: '300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-          },
-        }}
-      />
+      <div className="w-full bg-slate-900" sx={{ width: '100%' }}>
+        <MainButton
+          onClick={handleClickOpen}
+          startIcon={<FaPen />}
+          children="Whrite A Review"
+          sx={{
+            height: '50px',
+            color: 'rgb(250, 84, 28)',
+            '&.MuiCardActions-root': {
+              width: '100%',
+            },
+            '&:hover': {
+              boxShadow: '300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+            },
+          }}
+        />
+      </div>
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
