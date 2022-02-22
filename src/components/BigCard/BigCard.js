4f5ad4708690c4ebf78e22470c947c130e587562
galabeketov <galabeketov>
2022-02-22 13:13:23 +0500

    footer chala

--- src/components/BigCard/BigCard.js
+++ src/components/BigCard/BigCard.js
@@ -38,10 +38,7 @@ export default function BigCard({ obj, data }) {
               ${obj.price}
             </p>
           </div>
-          <NavLink
-            to={`courses/${obj.name}`}
-            onClick={() => console.log(obj.id)}
-          >
+          <NavLink to={`course`} onClick={() => console.log(obj.id)}>
             <h3 className="text-slate-800 pb-2 font-bold text-md">
               {obj.name}
             </h3>
